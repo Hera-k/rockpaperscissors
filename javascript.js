acf2c5bab37bf9eac8f300b949fae1cd5bb2187f
Karla Alaro <karlahera03@gmail.com>
2025-01-17 19:24:49 +0300

    Added two functions, playRound() to play the game to get a winner and increment the scores and playGame() to play 5 rounds

--- javascript.js
+++ javascript.js
@@ -17,3 +17,35 @@ function getHumanChoice(){
 //Keep track of scores
 let compScore = 0;
 let userScore = 0;
+
+
+
+//function to play a round
+function playRound(compChoice, userChoice){
+    if((userChoice === "rock" && compChoice === "scissors") || (userChoice === "paper" && compChoice === "rock") || (userChoice === "scissors" && compChoice === "paper")){
+        userScore ++;
+    } else if((userChoice === "rock" && compChoice === "paper") || (userChoice === "paper" && compChoice === "scissors") || (userChoice === "scissors" && compChoice === "rock")){
+        compScore++;
+    } else if (userChoice == compChoice){
+        compScore++;
+        userScore++;
+    }
+}
+
+//function to play the game, 5 rounds each
+function playGame(){
+    //get the Computer and user choice
+    let compChoice = getComputerChoice().toLowerCase();
+    let userChoice = getHumanChoice().toLowerCase();
+    for(let i = 0; i < 5; i++){
+        compChoice = getComputerChoice().toLowerCase();
+        userChoice = getHumanChoice().toLowerCase();
+        playRound(compChoice, userChoice);
+        console.log("The user chose " + userChoice);
+        console.log("The computer chose " + compChoice);    
+    }
+}
+
+playGame();
+console.log(userScore);
+console.log(compScore);

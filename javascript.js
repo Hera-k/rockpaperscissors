1c44cba503d768ed65cb27c9252f4ce189a98f88
Karla Alaro <karlahera03@gmail.com>
2025-01-17 18:10:40 +0300

    Created the function getComputerChoice in the .js file Moved script element from the body to the head
    This fixes the problem where the .js file was not linked

--- javascript.js
+++ javascript.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+function getComputerChoice(){
+    let choice = Math.floor(Math.random() * 100);
+    if(choice % 2 == 0){
+        return "Rock";
+    } else if(choice % 3 == 0){
+        return "Scissors";
+    } else{
+        return "Paper";
+    }
+}
+console.log(getComputerChoice());

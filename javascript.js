480c5083fd05d51e2d5311a5feec2d99ad593d13
Karla Alaro <karlahera03@gmail.com>
2025-01-17 18:16:11 +0300

    Created variables to keep score

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+//Get the computers choice
 function getComputerChoice(){
     let choice = Math.floor(Math.random() * 100);
     if(choice % 2 == 0){
@@ -8,7 +9,11 @@ function getComputerChoice(){
         return "Paper";
     }
 }
+//Get the users  choice
 function getHumanChoice(){
     let input = prompt("Choose between Rock, Paper or Scissors");
     return input;
 }
+//Keep track of scores
+let compScore = 0;
+let userScore = 0;
